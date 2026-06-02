4e945aaebcc15f92759fd49442f106dcca072304
benjaminagboola <info.bluejanuary@gmail.com>
2026-06-01 18:24:55 -0600

    updated config

---
+++ app/prisma.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'prisma/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+export default defineConfig({
+  earlyAccess: true,
+  schema: 'prisma/schema.prisma',
+  migrate: {
+    async adapter(env) {
+      return new PrismaPg({ connectionString: env.DIRECT_URL! });
+    },
+  },
+});
